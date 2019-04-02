2b012d43cb1a41e2fa23e883c01c492e5ee263ba
Irvan Ariyanto <irvan.ariyanto104@gmail.com>
2019-04-02 15:55:24 +0700

    Article Endpoint and Register

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,11 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('articles', 'ArticleController@index');
+Route::get('articles/{article}', 'ArticleController@show');
+Route::post('articles', 'ArticleController@store');
+Route::put('articles/{article}', 'ArticleController@update');
+Route::delete('articles/{article}', 'ArticleController@delete');
+
+Route::post('register', 'Auth\RegisterController@register');
